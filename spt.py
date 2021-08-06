99e3e8bcd5ad802a7f4422a21548d3e538e4eb29
Andrey Sobol <andreysobol@protonmail.com>
2021-08-06 04:55:51 +0300

    fixes

--- spt.py
+++ spt.py
@@ -16,12 +16,14 @@ class SparseMerkleTree():
 
         def calculate_level(levels, iteration):
             prev_level = levels[iteration]
-            new_level = [sha256(prev_level[i] + prev_level[i+1]).digest() for i in range(0, prev_level // 2)]
+            iterator = range(0, len(prev_level) // 2)
+            s = sha256(prev_level[0] + prev_level[0+1]).digest()
+            new_level = [sha256(prev_level[i] + prev_level[i+1]).digest() for i in iterator]
             return levels + [new_level]
 
         def calculate_full_tree(elememts, depth):
             hashed_elements = [sha256(element).digest() for element in elements]
-            reduce(calculate_level, range(0, depth-1), hashed_elements)
+            return reduce(calculate_level, range(0, depth), [hashed_elements])
 
         self.lists = calculate_full_tree(self.elements, depth)
 
