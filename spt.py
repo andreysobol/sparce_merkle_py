e58a297492fb53b95145528e4374f5eac6830bd3
Andrey Sobol <andreysobol@protonmail.com>
2021-09-20 18:00:57 +0300

    todo -> done

--- spt.py
+++ spt.py
@@ -113,7 +113,6 @@ class SparseMerkleTree():
 
     def remove_element(self, index: int) -> None:
         if index in self.elements:
-            # TODO: check correctness
             self.modify_element(index, self.empty_element)
         else:
             raise Exception("Value doesn't exist")
